f9074d3b97298b39885abbe7ae3c3bd7822b39cf
Dwyriel <dwyriel333@hotmail.com>
2023-01-20 00:15:59 -0300

    Transforming LoginDiv by calculated offset

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,18 +1,22 @@
-import {AfterViewInit, Component, ElementRef, OnInit} from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+import { fromEvent, Observable, Subscription } from "rxjs";
 
 @Component({
     selector: 'app-login',
     templateUrl: './login.page.html',
     styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit, AfterViewInit {
+export class LoginPage {
+
+    @ViewChild('LoginDiv') LoginDiv?: ElementRef;
+
+    private loginDivHeightObservable?: Observable<Event>;
+    private loginDivHeightSubscription?: Subscription;
 
     constructor() { }
 
-    ngAfterViewInit() {
+    ionViewDidEnter() {
+        this.LoginDiv?.nativeElement.style.setProperty("--calculatedOffset", ((this.LoginDiv?.nativeElement.offsetHeight / 2) * -1) + "px");
     }
-
-    ngOnInit() {
-    }
-
 }
