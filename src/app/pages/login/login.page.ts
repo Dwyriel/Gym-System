a27bce036022274f20c69b2ef455ef2d3d905121
Eduardo P <edtvports@gmail.com>
2023-01-20 19:31:33 -0300

    Auth errors and better submit button

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -2,6 +2,9 @@ import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angula
 
 import { fromEvent, Observable, Subscription } from "rxjs";
 
+import { AccountService } from "../../services/account.service";
+import {user} from "@angular/fire/auth";
+
 @Component({
     selector: 'app-login',
     templateUrl: './login.page.html',
@@ -10,20 +13,54 @@ import { fromEvent, Observable, Subscription } from "rxjs";
 export class LoginPage {
 
     @ViewChild('LoginDiv') LoginDiv?: ElementRef;
+    @ViewChild('SubmitButton') SubmitButton?: ElementRef;
+    @ViewChild('Message') Message?: ElementRef;
+
 
     private loginDivHeightObservable?: Observable<Event>;
     private loginDivHeightSubscription?: Subscription;
 
-    username: String = "";
-    password: String = "";
+    username: string = "";
+    password: string = "";
 
-    constructor() { }
+    inputIsEmpty: boolean = true;
+
+    constructor(private accountService: AccountService) { }
 
     ionViewDidEnter() {
         this.LoginDiv?.nativeElement.style.setProperty("--calculatedOffset", ((this.LoginDiv?.nativeElement.offsetHeight / 2) * -1) + "px");
     }
 
-    submitCredentials() {
+    LoginBtn() {
+        this.accountService.Login(this.username, this.password)
+            .then()
+            .catch(error => {
+                this.Message?.nativeElement.style.setProperty("color", "var(--ion-color-danger)");
+                this.DisplayErrorMessage(error.code);
+        });
 
+        this.username = "";
+        this.password = "";
+    }
+
+    DisplayErrorMessage(errorCode: string) {
+        switch (errorCode) {
+            case "auth/invalid-email":
+            case "auth/user-not-found":
+                this.Message!.nativeElement.textContent = "Email inválido";
+                break;
+            case "auth/wrong-password":
+                this.Message!.nativeElement.textContent = "Senha inválida";
+                break;
+            case "auth/network-request-failed":
+                this.Message!.nativeElement.textContent = "Sem conexão";
+                break;
+            default:
+                this.Message!.nativeElement.textContent = errorCode;
+        }
+    }
+
+    ChangeOnInput() {
+        setTimeout(() => {this.inputIsEmpty = !(this.username != "" && this.password != "")}, 10);
     }
 }
