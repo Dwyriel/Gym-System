bb30576b7a3a3118c4f118c2c5675cf908f94335
Eduardo P <edtvports@gmail.com>
2023-01-19 23:33:35 -0300

    Le Commit

---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,18 @@
+import {AfterViewInit, Component, ElementRef, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-login',
+    templateUrl: './login.page.html',
+    styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit, AfterViewInit {
+
+    constructor() { }
+
+    ngAfterViewInit() {
+    }
+
+    ngOnInit() {
+    }
+
+}
