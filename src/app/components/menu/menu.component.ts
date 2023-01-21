8afae444fa2e907c41ca8910c651e3b500f18a66
Eduardo P <edtvports@gmail.com>
2023-01-21 20:14:25 -0300

    Account and device info

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { AccountService } from "../../services/account.service";
 import { MenuController } from "@ionic/angular";
 import {Router} from "@angular/router";
@@ -9,10 +9,17 @@ import {Router} from "@angular/router";
   styleUrls: ['./menu.component.scss'],
 })
 export class MenuComponent implements OnInit {
+    displayUsername: string | null | undefined;
 
     constructor(private router: Router, private accountService: AccountService, private menu: MenuController) { }
 
-    ngOnInit() {}
+    ngOnInit() {
+        this.accountService.GetUserObservable().subscribe(result => {
+            if (result && typeof result != "boolean") {
+                this.displayUsername = (result?.displayName) ? result?.displayName : result?.email;
+            }
+        });
+    }
 
     async LogoutBtn() {
         await this.menu.close("menu");
