3eff82088f240248ab0145b3ed00b315b86a45e3
Eduardo P <edtvports@gmail.com>
2023-01-21 12:20:16 -0300

    Startup page

--- src/app/pages/startup/startup.page.ts
+++ src/app/pages/startup/startup.page.ts
@@ -1,4 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {gymName} from '../../../environments/environment';
+import {AppInfoService} from "../../services/app-info.service";
+import {Subscription} from "rxjs";
 
 @Component({
     selector: 'app-startup',
@@ -6,8 +9,29 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./startup.page.scss'],
 })
 export class StartupPage implements OnInit {
+    @ViewChild('SpinnerDiv') SpinnerDivElement?: ElementRef;
+
+    readonly gymName: string = gymName;
+
+    private appInfoSubscription?: Subscription;
 
     constructor() { }
+
+    ionViewDidEnter() {
+        if (this.appInfoSubscription && !this.appInfoSubscription.closed)
+            this.appInfoSubscription.unsubscribe();
+        this.appInfoSubscription = AppInfoService.GetAppInfoObservable().subscribe(appInfo => {
+            if (!appInfo)
+                return
+            this.SpinnerDivElement!.nativeElement.style.setProperty("--calculatedOffsetY", ((this.SpinnerDivElement?.nativeElement.offsetHeight / 2) * -1) - 40 + "px");
+            this.SpinnerDivElement!.nativeElement.style.setProperty("--calculatedOffsetX", (appInfo.appWidth >= 600) ? (((this.SpinnerDivElement?.nativeElement.offsetWidth / 2) * -1) + "px") : "-50%");
+        });
+    }
+
+    ionViewWillLeave() {
+        if (this.appInfoSubscription && !this.appInfoSubscription.closed)
+            this.appInfoSubscription.unsubscribe();
+    }
 
     ngOnInit() {
     }
