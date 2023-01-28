97685d91a703d82abe013c583c9f66f6ee3fda37
Dwyriel <dwyriel333@hotmail.com>
2023-01-27 23:21:45 -0300

    Half-assed fix

--- src/app/pages/exercises/exercises.page.ts
+++ src/app/pages/exercises/exercises.page.ts
@@ -5,9 +5,9 @@ import {PractitionerService} from "../../services/practitioner.service";
 import {AlertService} from "../../services/alert.service";
 
 @Component({
-  selector: 'app-exercises',
-  templateUrl: './exercises.page.html',
-  styleUrls: ['./exercises.page.scss'],
+    selector: 'app-exercises',
+    templateUrl: './exercises.page.html',
+    styleUrls: ['./exercises.page.scss'],
 })
 export class ExercisesPage {
     //TODO: LIMIT HTML SIZE
@@ -17,12 +17,12 @@ export class ExercisesPage {
 
     public searchFilter: string = "";
 
-
     constructor(private exercisesService: ExercisesService, private practitionersService: PractitionerService, private alertService: AlertService) { }
 
     async ionViewWillEnter() {
         await this.PopulateInterface();
     }
+
     ionViewDidLeave() {
         this.exercisesByCategory = new Array<ExercisesByCategory>();
         this.allExercisesByCategory = new Array<ExercisesByCategory>();
@@ -30,24 +30,25 @@ export class ExercisesPage {
         this.searchFilter = "";
     }
 
+    stringy: string = "";
+
     async PopulateInterface() {
         this.allExercises = await this.exercisesService.GetAllExercises();
         let categories: Array<string> = await this.exercisesService.GetAllCategories(this.allExercises);
         categories.forEach(categoryName => this.exercisesByCategory.push({categoryName, exercises: new Array<ExerciseTemplate>()}));
 
-        for (let i = 0; i < categories.length; i++){
+        for (let i = 0; i < categories.length; i++) {
             this.allExercises.forEach(exercise => {
                 if (exercise.category == this.exercisesByCategory[i].categoryName)
                     this.exercisesByCategory[i].exercises.push(exercise);
             });
         }
-        this.allExercisesByCategory = this.exercisesByCategory;
+        this.stringy = JSON.stringify(this.exercisesByCategory);
+        this.allExercisesByCategory = JSON.parse(this.stringy);
     }
 
     RepopulateInterface() {
-        this.exercisesByCategory = Array.from(this.allExercisesByCategory);
-        console.log(this.exercisesByCategory);
-        console.log(this.allExercisesByCategory);
+        this.exercisesByCategory = JSON.parse(this.stringy);
     }
 
     async DeleteExerciseBtn(exercise: ExerciseTemplate) {
@@ -65,8 +66,7 @@ export class ExercisesPage {
                             this.exercisesByCategory.splice(categoriesIndex, 1);
                     }
                 });
-            }
-            else
+            } else
                 await this.alertService.ShowToast("Exercício não pode ser apagado", undefined, "danger");
             this.searchFilter = "";
             this.allExercisesByCategory = this.exercisesByCategory;
@@ -91,7 +91,8 @@ export class ExercisesPage {
         this.RepopulateInterface();
         for (let i = 0; i < this.exercisesByCategory.length; i++) {
             for (let j = 0; j < this.exercisesByCategory[i].exercises.length; j++) {
-                if (!this.exercisesByCategory[i].exercises[j].name.includes(this.searchFilter)) {
+                let name = this.exercisesByCategory[i].exercises[j].name.toLowerCase().normalize("NFD").replace(/\p{Diacritic}/gu, "");
+                if (!name.includes(this.searchFilter.toLowerCase().normalize("NFD").replace(/\p{Diacritic}/gu, ""))) {
                     this.exercisesByCategory[i].exercises.splice(j, 1);
                     j--;
                 }
@@ -101,11 +102,6 @@ export class ExercisesPage {
                 i--;
             }
         }
-    }
-
-    async ClearFilterAndSearch() {
-        this.searchFilter = "";
-        await this.RepopulateInterface();
     }
 }
 
