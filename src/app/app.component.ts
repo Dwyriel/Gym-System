8791750397da974ab245c643851cac3cb97aa15d
Dwyriel <dwyriel333@hotmail.com>
2023-02-01 18:26:10 -0300

    AppInfo.isOnline && Network plugin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {MenuController, Platform} from "@ionic/angular";
+import {Router} from "@angular/router";
+import {Network} from '@capacitor/network';
 import {Subscription} from "rxjs";
 import {gymName} from '../environments/environment';
 import {AppInfoService} from "./services/app-info.service";
 import {DeviceIDService} from "./services/device-id.service";
 import {getRemSizeInPixels, inverseLerp, UnsubscribeIfSubscribed} from "./services/app.utility";
 import {Themes} from "./classes/app-config";
-import {Router} from "@angular/router";
 import {AccountService} from "./services/account.service";
+import {AppInfo} from "./interfaces/app-info";
 
 const handleColorSchemeChangeEvent = (event: MediaQueryListEvent) => document.body.classList.toggle("dark", event.matches);
 
@@ -20,6 +22,7 @@ export class AppComponent implements OnInit, OnDestroy {//TODO Save stuff on fir
     private readonly maxMobileWidth = 1024;
     private readonly paddingSizeInRem = 3;
     private sysTheme?: MediaQueryList;
+    private cachedAppInfo: AppInfo = {appWidth: 1000, appHeight: 1000, maxMobileWidth: this.maxMobileWidth, userAgent: navigator.userAgent, isMobile: true, isOnline: true};
 
     private resizeSubscription?: Subscription;
     private appConfigSubscription?: Subscription;
@@ -36,9 +39,11 @@ export class AppComponent implements OnInit, OnDestroy {//TODO Save stuff on fir
     constructor(private platform: Platform, private deviceIDService: DeviceIDService, private router: Router, private accountService: AccountService, private menuController: MenuController) {}
 
     async ngOnInit() {
-        this.GetPlatformInfo();
+        this.SetPlatformInfo();
         await this.SetAppTheme();
         await this.SetDeviceName();
+        await this.CheckForConnectivity();
+        AppInfoService.PushAppInfo(this.cachedAppInfo);
         this.CheckIfUserIsLoggedInAndRedirect();
     }
 
@@ -47,27 +52,29 @@ export class AppComponent implements OnInit, OnDestroy {//TODO Save stuff on fir
         UnsubscribeIfSubscribed(this.appConfigSubscription);
         UnsubscribeIfSubscribed(this.accountSubscription);
         UnsubscribeIfSubscribed(this.deviceIDSubscription);
+        await Network.removeAllListeners();
     }
 
-    GetPlatformInfo() {
-        this.PushAppInfoAndSetCSS();
+    SetPlatformInfo() {
+        this.SetNewWindowSize();
+        this.SetCSSProperties(this.cachedAppInfo);
         UnsubscribeIfSubscribed(this.resizeSubscription);
         this.resizeSubscription = this.platform.resize.subscribe(() => {
-            this.PushAppInfoAndSetCSS();
+            this.SetNewWindowSize();
+            this.SetCSSProperties(this.cachedAppInfo);
+            AppInfoService.PushAppInfo(this.cachedAppInfo);
         });
     }
 
-    PushAppInfoAndSetCSS() {
-        let appInfo = {
-            appWidth: this.platform.width(),
-            appHeight: this.platform.height(),
-            userAgent: navigator.userAgent,
-            isMobile: this.platform.width() <= this.maxMobileWidth,
-            maxMobileWidth: this.maxMobileWidth
-        }
+    SetNewWindowSize() {
+        this.cachedAppInfo.appWidth = this.platform.width();
+        this.cachedAppInfo.appHeight = this.platform.height();
+        this.cachedAppInfo.isMobile = this.maxMobileWidth > this.platform.width();
+    }
+
+    SetCSSProperties(appInfo: AppInfo) {
         document.documentElement.style.setProperty("--mobile-max-width", appInfo.maxMobileWidth + "px");
         document.documentElement.style.setProperty("--desktop-padding-top", appInfo?.isMobile ? "0" : ((inverseLerp(appInfo!.appWidth, appInfo!.maxMobileWidth, appInfo!.maxMobileWidth + (getRemSizeInPixels() * (this.paddingSizeInRem + this.paddingSizeInRem))) * this.paddingSizeInRem) + "rem"));
-        AppInfoService.PushAppInfo(appInfo);
     }
 
     async SetAppTheme() {
@@ -99,6 +106,14 @@ export class AppComponent implements OnInit, OnDestroy {//TODO Save stuff on fir
             this.deviceName = deviceName
             this.isLoadingDeviceName = false;
         });
+    }
+
+    async CheckForConnectivity() {
+        this.cachedAppInfo.isOnline = (await Network.getStatus()).connected;
+        Network.addListener("networkStatusChange", status => {
+            this.cachedAppInfo.isOnline = status.connected;
+            AppInfoService.PushAppInfo(this.cachedAppInfo);
+        })
     }
 
     CheckIfUserIsLoggedInAndRedirect() {
