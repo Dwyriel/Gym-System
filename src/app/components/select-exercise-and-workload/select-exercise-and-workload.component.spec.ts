04ddeb019f5db0dfb31750dd70d91be96dbcc257
Dwyriel <dwyriel333@hotmail.com>
2023-02-28 18:07:09 -0300

    Select Exercise And Workload Component

---
+++ src/app/components/select-exercise-and-workload/select-exercise-and-workload.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { SelectExerciseAndWorkloadComponent } from './select-exercise-and-workload.component';
+
+describe('SelectExerciseAndWorkloadComponent', () => {
+  let component: SelectExerciseAndWorkloadComponent;
+  let fixture: ComponentFixture<SelectExerciseAndWorkloadComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ SelectExerciseAndWorkloadComponent ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(SelectExerciseAndWorkloadComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,6 +1,4 @@
 import {Component} from '@angular/core';
-import {Router} from "@angular/router";
-import {AccountService} from "../../services/account.service";
 
 @Component({
     selector: 'app-home',
