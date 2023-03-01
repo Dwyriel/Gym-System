eddee00b46bdd827db5b592f456eb50c586f01c7
Dwyriel <dwyriel333@hotmail.com>
2023-03-01 06:21:17 -0300

    PracProfile startup fixes & Refactoring for better UX

--- src/app/pages/practitioner-profile/practitioner-profile.page.ts
+++ src/app/pages/practitioner-profile/practitioner-profile.page.ts
@@ -3,6 +3,8 @@ import {ActivatedRoute, Router} from "@angular/router";
 import {Practitioner} from "../../classes/practitioner";
 import {PractitionerService} from "../../services/practitioner.service";
 import {AlertService} from "../../services/alert.service";
+import {AccountService} from "../../services/account.service";
+import {waitForFirebaseResponse} from "../../services/app.utility";
 
 @Component({
     selector: 'app-practitioner-profile',
@@ -12,17 +14,20 @@ import {AlertService} from "../../services/alert.service";
 export class PractitionerProfilePage {
     public practitionerInfo?: Practitioner;
     public isLoading: boolean = false;
-    public practitionerID?: string | null;
+    public practitionerID: string | null = null;
 
-    constructor(private activatedRoute: ActivatedRoute, private practitionerService: PractitionerService, private alertService: AlertService, private router: Router) { }
+    constructor(private router: Router, private activatedRoute: ActivatedRoute, private accountService: AccountService, private practitionerService: PractitionerService, private alertService: AlertService) { }
 
     async ionViewWillEnter() {
         this.isLoading = true;
+        if (!(await waitForFirebaseResponse(this.accountService)))
+            return;
         let id = await this.alertService.PresentLoading("Carregando");
         this.practitionerID = this.activatedRoute.snapshot.paramMap.get("id");
-        this.practitionerService.GetPractitioner(this.practitionerID!)
-            .then(result => { this.practitionerInfo = result; this.isLoading = false; })
-            .catch(async () => await this.router.navigate(['practitioner-list']));
+        await this.practitionerService.GetPractitioner(this.practitionerID!).then(result => {
+            this.practitionerInfo = result;
+            this.isLoading = false;
+        }).catch(async () => await this.router.navigate(['practitioner-list']));
         await this.alertService.DismissLoading(id);
     }
 
@@ -36,13 +41,13 @@ export class PractitionerProfilePage {
         let confirmation = await this.alertService.ConfirmationAlert("Deseja deletar este aluno?", `"${this.practitionerInfo?.name}" desaparecerá para sempre`, "Não", "Sim");
         if (!confirmation)
             return;
-        this.practitionerService.DeletePractitioner(this.practitionerID!)
-            .then(async () => {
-            this.isLoading = true;
+        let id = await this.alertService.PresentLoading("Carregando");
+        this.isLoading = true;
+        await this.practitionerService.DeletePractitioner(this.practitionerID!).then(async () => {
             await this.router.navigate(["/practitioner-list"])
             await this.alertService.ShowToast("Aluno apagado com sucesso", undefined, "primary");
-            this.isLoading = false;
-            })
-            .catch(async () => await this.alertService.ShowToast("Aluno não pode ser apagado", undefined, "danger"));
+        }).catch(async () => await this.alertService.ShowToast("Aluno não pode ser apagado", undefined, "danger"));
+        this.isLoading = false;
+        await this.alertService.DismissLoading(id);
     }
 }
