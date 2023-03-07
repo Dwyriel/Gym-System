c4b1a3151428f780f48686d051205edae3dfc3e8
Dwyriel <dwyriel333@hotmail.com>
2023-03-07 16:16:28 -0300

    Working template-add page

--- src/app/pages/template-add/template-add.page.ts
+++ src/app/pages/template-add/template-add.page.ts
@@ -11,6 +11,24 @@ import {Exercise, PractitionerExercise} from "../../interfaces/exercise";
 import {SelectExerciseAndWorkloadComponent} from "../../components/select-exercise-and-workload/select-exercise-and-workload.component";
 import {PopoverController} from "@ionic/angular";
 
+interface internalTemplateExercise {
+    checked: boolean;
+    exercise: Exercise;
+    workload: { series?: number, repetition?: number, rest?: number, load?: number };
+}
+
+function sortByName(first: ExerciseTemplate, second: ExerciseTemplate) {
+    let firstLowerCase = first.name.toLowerCase();
+    let secondLowerCase = second.name.toLowerCase();
+    return (firstLowerCase < secondLowerCase) ? -1 : (firstLowerCase > secondLowerCase) ? 1 : 0;
+}
+
+function sortByCategory(first: internalTemplateExercise, second: internalTemplateExercise) {
+    let firstLowerCase = first.exercise.category.toLowerCase();
+    let secondLowerCase = second.exercise.category.toLowerCase();
+    return (firstLowerCase < secondLowerCase) ? -1 : (firstLowerCase > secondLowerCase) ? 1 : 0;
+}
+
 @Component({
     selector: 'app-template-add',
     templateUrl: './template-add.page.html',
@@ -18,84 +36,116 @@ import {PopoverController} from "@ionic/angular";
 })
 export class TemplateAddPage {
     public practitionerID: string | null = null;
-    public practitionerInfo?: Practitioner;
+    public practitioner?: Practitioner;
     public templatesList: ExerciseTemplate[] = [];
-    public selectedTemplateName = "";
-    public selectedTemplate?: ExerciseTemplate;
-    public checkedExercises: PractitionerExercise[] = [];
-    public isLoading = false;
+    public selectedTemplateName?: string;
+    public templateExercises: internalTemplateExercise[] = [];
+    public isLoading = true;
 
     constructor(private accountService: AccountService, private activatedRoute: ActivatedRoute, private practitionerService: PractitionerService, private exercisesService: ExercisesService, private alertService: AlertService, private router: Router, private popoverController: PopoverController) { }
 
     async ionViewWillEnter() {
-        this.isLoading = true;
         if (!(await waitForFirebaseResponse(this.accountService)))
             return;
         this.practitionerID = this.activatedRoute.snapshot.paramMap.get("id");
-        let cacheError = false, errorOccurred = false;
-        await this.practitionerService.GetPractitionerFromCache(this.practitionerID!).then(result => this.practitionerInfo = result).catch(() => cacheError = true);
-        if(cacheError)
-            await this.practitionerService.GetPractitioner(this.practitionerID!).then(result => this.practitionerInfo = result).catch(() => errorOccurred = true);
+        let errorOccurred = await this.getPractitioner();
+        if (!errorOccurred)
+            errorOccurred = await this.getTemplates();
         if (errorOccurred) {
             await this.alertService.ShowToast("Ocorreu um erro carregando as informações", undefined, "danger");
             return;
         }
-
-        this.templatesList = await this.exercisesService.GetAllExerciseTemplates();
-        for (let template of this.templatesList) {
-            template.exercises = [];
-            for (let id of template.exerciseIDs)
-                template.exercises?.push(await this.exercisesService.GetExercise(id));
-        }
-
-
         this.isLoading = false;
     }
 
     ionViewDidLeave() {
+        this.practitioner = undefined;
+        this.templatesList = [];
+        this.selectedTemplateName = undefined;
+        this.templateExercises = [];
+        this.isLoading = true;
     }
 
-    public changedTemplate(name: string) {
-        for (let template of this.templatesList)
-            if (template.name == name) {
-                this.selectedTemplate = template;
-                return;
-            }
+    async getPractitioner() {
+        let cacheError = false, errorOccurred = false;
+        await this.practitionerService.GetPractitionerFromCache(this.practitionerID!).then(result => this.practitioner = result).catch(() => cacheError = true);
+        if (cacheError)
+            await this.practitionerService.GetPractitioner(this.practitionerID!).then(result => this.practitioner = result).catch(() => errorOccurred = true);
+        return errorOccurred;
     }
 
-    public async OnCheck(exercise: Exercise) {
-        for (let [index, checkedExercise] of this.checkedExercises.entries()) {
-            if (checkedExercise.exerciseID == exercise.thisObjectID) {
-                this.checkedExercises.splice(index, 1);
-                console.log(this.checkedExercises);
-                return;
-            }
+    async getTemplates() {
+        let errorOccurred = false;
+        this.templatesList = await this.exercisesService.GetAllExerciseTemplates();
+        this.templatesList.sort(sortByName);
+        for (let template of this.templatesList) {
+            await this.exercisesService.GetTemplatesExercises(template.exerciseIDs).then(value => template.exercises = value).catch(() => errorOccurred = true);
+            if (errorOccurred)
+                break;
         }
+        return errorOccurred;
+    }
 
+    get checkIfSomethingIsntValid() {
+        return !this.selectedTemplateName || !this.templateExercises.some(value => value.checked);
+    }
+
+    changedTemplate() {
+        this.templateExercises = [];
+        for (let template of this.templatesList)
+            if (template.name === this.selectedTemplateName) {
+                for (let exercise of template.exercises!)
+                    this.templateExercises.push({checked: false, exercise: exercise, workload: {series: undefined, repetition: undefined, rest: undefined, load: undefined}});
+                this.templateExercises.sort(sortByCategory);
+                return;
+            }
+    }
+
+    async OnCheck(event: any, index: number) {
+        if (!this.templateExercises[index].checked)
+            return;
         const editExercisePopover = await this.popoverController.create({
             component: SelectExerciseAndWorkloadComponent,
             mode: 'md',
-            componentProps: {workloadInput: {series: 1, repetition: 1, rest: 0, load: 0}},
+            componentProps: {workloadInput: {...this.templateExercises[index].workload}},
             animated: true
         });
         editExercisePopover.onDidDismiss().then(async value => {
-            let newExercise: PractitionerExercise = {
-                exerciseID: exercise.thisObjectID!,
-                exercise: exercise,
-                series: 1,
-                repetition: 1,
-                rest: 0,
-                load: 0
-            };
-            if (value.data) {
-                newExercise.series = value.data.updatedWorkload!.series;
-                newExercise.repetition = value.data.updatedWorkload!.repetition;
-                newExercise.rest = value.data.updatedWorkload!.rest;
-                newExercise.load = value.data.updatedWorkload!.load;
+            if (!value.data || value.data && (!value.data.updatedWorkload.series || !value.data.updatedWorkload.repetition || !value.data.updatedWorkload.rest || !value.data.updatedWorkload.load)) {
+                this.templateExercises[index].checked = !this.templateExercises[index].checked;
+                return;
             }
-            this.checkedExercises.push(newExercise);
+            let workload: { series?: number, repetition?: number, rest?: number, load?: number } = value.data.updatedWorkload;
+            this.templateExercises[index].workload.series = workload.series;
+            this.templateExercises[index].workload.repetition = workload.repetition;
+            this.templateExercises[index].workload.rest = workload.rest;
+            this.templateExercises[index].workload.load = workload.load;
         });
         await editExercisePopover.present();
     }
 
+    async OnClick() {
+        let loadingID = await this.alertService.PresentLoading("Carregando");
+        let exercisesToAdd: PractitionerExercise[] = [];
+        for (let exercise of this.templateExercises) {
+            if (!exercise.checked)
+                continue;
+            exercisesToAdd.push({exerciseID: exercise.exercise.thisObjectID!, series: exercise.workload.series!, repetition: exercise.workload.repetition!, rest: exercise.workload.rest!, load: exercise.workload.load!});
+        }
+        let errorOccurred = false;
+        await this.practitionerService.ClearExercises(this.practitioner!.exercisesID).catch(() => errorOccurred = true);
+        if (!errorOccurred)
+            await this.practitionerService.AddExercise(this.practitioner!.exercisesID, ...exercisesToAdd).catch(() => errorOccurred = true);
+        if (!errorOccurred)
+            await this.practitionerService.UpdatePractitioner(this.practitionerID!, {templateName: this.selectedTemplateName}).catch((err) => {
+                errorOccurred = true
+                console.log(err);
+            });
+        await this.alertService.DismissLoading(loadingID);
+        if (errorOccurred) {
+            await this.alertService.ShowToast("Ocorreu um erro atribuindo o ciclo", undefined, "danger");
+            return;
+        }
+        await this.alertService.ShowToast("Ciclo atribuido com sucesso", undefined, "primary");
+    }
 }
