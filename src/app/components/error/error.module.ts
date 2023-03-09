2fd4624fdd27dc6938dd7f189633f60e56a67ef4
Dwyriel <dwyriel333@hotmail.com>
2023-03-09 17:05:37 -0300

    Fix ErrorComponent routerLink

--- src/app/components/error/error.module.ts
+++ src/app/components/error/error.module.ts
@@ -3,12 +3,14 @@ import {ErrorComponent} from "./error.component";
 import {CommonModule} from "@angular/common";
 import {FormsModule} from "@angular/forms";
 import {IonicModule} from "@ionic/angular";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
     imports: [
         CommonModule,
         FormsModule,
         IonicModule,
+        RouterModule
     ],
     declarations: [
         ErrorComponent,
